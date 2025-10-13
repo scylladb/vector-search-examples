24e3ede26713521732aba948b40b8d7b67d8b069
Attila Toth <hello@attilatoth.dev>
2025-10-13 17:49:40 +0200

    streamlit cloud config

--- movie-recommendation/config.py
+++ movie-recommendation/config.py
@@ -1,3 +1,4 @@
+import os
 
 SCYLLADB_CONFIG = {
     "host": "node-0.aws-us-east-1.xxxxxxxx.clusters.scylla.cloud",
@@ -7,3 +8,14 @@ SCYLLADB_CONFIG = {
     "datacenter": "AWS_US_EAST_1",
     "keyspace": "recommend"
 }
+
+running_on_streamlit_cloud = bool(os.getenv("ON_STREAMLIT_CLOUD"))
+if running_on_streamlit_cloud.lower() == "true":
+    SCYLLADB_CONFIG = {
+        "host": os.getenv("SCYLLADB_HOST"),
+        "port": os.getenv("SCYLLADB_PORT"),
+        "username": os.getenv("SCYLLADB_USERNAME"),
+        "password": os.getenv("SCYLLADB_PASSWORD"),
+        "datacenter": os.getenv("SCYLLADB_DATACENTER"),
+        "keyspace": os.getenv("SCYLLADB_KEYSPACE")
+    }
