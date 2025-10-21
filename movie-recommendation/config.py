6132fddae9b1c3bf8982384bfbc3470d845e7e47
Attila Tóth <hello@attilatoth.dev>
2025-10-21 17:18:44 +0200

    Remove streamlit config

--- movie-recommendation/config.py
+++ movie-recommendation/config.py
@@ -8,14 +8,3 @@ SCYLLADB_CONFIG = {
     "datacenter": "AWS_US_EAST_1",
     "keyspace": "recommend"
 }
-
-running_on_streamlit_cloud = os.getenv("ON_STREAMLIT_CLOUD")
-if running_on_streamlit_cloud.lower() == "true":
-    SCYLLADB_CONFIG = {
-        "host": os.getenv("SCYLLADB_HOST"),
-        "port": os.getenv("SCYLLADB_PORT"),
-        "username": os.getenv("SCYLLADB_USERNAME"),
-        "password": os.getenv("SCYLLADB_PASSWORD"),
-        "datacenter": os.getenv("SCYLLADB_DATACENTER"),
-        "keyspace": os.getenv("SCYLLADB_KEYSPACE")
-    }
