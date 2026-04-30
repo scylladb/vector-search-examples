9a77f720f92476428056219d5abd3da24a1d3ae0
Attila Toth <attila.toth@scylladb.com>
2026-04-30 14:03:20 +0200

    restructure rag tutorials

--- rag-movie-chatbot/config.py
+++ rag-movie-chatbot/config.py
@@ -1,24 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SCYLLADB_CONFIG = {
-    "host": "node-0.aws-us-east-1.xxxxxxxxx.clusters.scylla.cloud",
-    "port": "9042",
-    "username": "scylla",
-    "password": "xxxxxxxxx",
-    "datacenter": "AWS_US_EAST_1",
-    "keyspace": "recommend"
+    "host": os.getenv("SCYLLADB_HOST"),
+    "port": os.getenv("SCYLLADB_PORT", "9042"),
+    "username": os.getenv("SCYLLADB_USERNAME", "scylla"),
+    "password": os.getenv("SCYLLADB_PASSWORD"),
+    "datacenter": os.getenv("SCYLLADB_DATACENTER"),
+    "keyspace": os.getenv("SCYLLADB_KEYSPACE", "recommend")
 }
 
-GROQ_API_KEY = "xxxxxxxxxxx"
-
-running_on_streamlit_cloud = os.getenv("ON_STREAMLIT_CLOUD")
-if running_on_streamlit_cloud and running_on_streamlit_cloud.lower() == "true":
-    SCYLLADB_CONFIG = {
-        "host": os.getenv("SCYLLADB_HOST"),
-        "port": os.getenv("SCYLLADB_PORT"),
-        "username": os.getenv("SCYLLADB_USERNAME"),
-        "password": os.getenv("SCYLLADB_PASSWORD"),
-        "datacenter": os.getenv("SCYLLADB_DATACENTER"),
-        "keyspace": os.getenv("SCYLLADB_KEYSPACE")
-    }
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
